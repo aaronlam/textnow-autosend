a6f65e8c0a052c92972eca6a3a947bebbc0300f2
ParalysedFlower <48697066+ParalysedFlower@users.noreply.github.com>
2020-11-24 17:46:36 +0800

    Delete index.js

--- config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  username: process.env.TEXTNOW_USERNAME || "",
-  password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
-  message: process.env.TEXTNOW_MESSAGE || "autosend message",
-};
