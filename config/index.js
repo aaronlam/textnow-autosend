b4a5e3cdd36295843ecf1c6ec136bd69b0bf7c63
jingshr <91071619+jingshr@users.noreply.github.com>
2021-09-20 22:00:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   username: process.env.TEXTNOW_USERNAME || '',
   password: process.env.TEXTNOW_PASSWORD || '',
   cookies: process.env.TEXTNOW_COOKIES || '',
-  recipient: process.env.TEXTNOW_RECIPIENT || '(726) 666-0002',
+  recipient: process.env.TEXTNOW_RECIPIENT || '(516) 855-5259',
   message: process.env.TEXTNOW_MESSAGE || 'auto-send message',
   captchaToken: process.env.CAPTCHA_TOKEN || '',
 };
